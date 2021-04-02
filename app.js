58c294fc55e9c54b65f53b09abe788ee20d51abf
Sam Lord <hello@samlord.me>
2021-04-02 16:28:53 +0100

    Introduce new functionality and testing through docker
    no issue
    
    Wasn't sure how else to test this, since it needs a web server on port
    80, and a domain. Currently playing with a couple of ideas so this is
    all still a WIP.

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
+const express = require('express');
+const apiApp = express();
+const challengeApp = express();
+
+const config = require('./lib/config');
+const logging = require('./lib/logging');
+const challengeResponder = require('./lib/challenge-responder');
+const apiRoutes = require('./lib/api');
 const greenlock = require('./lib/greenlock');
 
+/**
+ * Main functionality includes:
+ * - setting up HTTP server to respond to ACME challenges
+ * - setting up HTTP server to respond to API requests (separate port)
+ * - starting Greenlock
+ */
 async function main() {
-    greenlock.sites.add({
-        subject: "example.com",
-        altnames: ["example.com", "www.example.com"]
-    });
+    challengeApp.use(challengeResponder);
+    challengeApp.listen(config.get('challengeResponder:port'));
+    logging.info(`ACME challenge app listening on port ${config.get('challengeResponder:port')}`);
+
+    apiApp.use('/api', apiRoutes);
+    apiApp.listen(config.get('api:port'));
+    logging.info(`Domain API listening on port ${config.get('api:port')}`);
+
+    // Initialise greenlock
+    await greenlock();
 }
 
 main();
