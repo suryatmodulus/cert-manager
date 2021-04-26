df534c8f996b307a3e9d021b2a8793a6bdfa0f36
Sam Lord <hello@samlord.me>
2021-04-26 10:53:34 +0100

    Add initial unit test for API functions
    no issue
    
    Need to add some more of these, but this is a start

---
+++ test/unit/api.test.js
@@ -0,0 +1,53 @@
+const {getHmac} = require('../utils');
+
+const request = require('supertest');
+const app = require('express')();
+const apiRoutes = require('../../lib/api');
+const GreenlockService = require('../../lib/greenlock-service');
+
+describe('API functions correctly', function () {
+    before(function () {
+        const certs = {};
+        const greenlock = new GreenlockService({
+            greenlock: {
+                async add({subject}) {
+                    certs[subject] = {
+                        content: 'hello!'
+                    }
+                    return Promise.resolve({
+                        success: true
+                    });
+                },
+
+                async get({servername}) {
+                    const cert = certs[servername];
+                    if (!cert || cert.deletedAt) {
+                        return Promise.resolve(null);
+                    } else {
+                        return Promise.resolve(cert);
+                    }
+                },
+
+                manager: {
+                    async set({subject, deletedAt}) {
+                        certs[subject].deletedAt = deletedAt;
+                        return Promise.resolve({
+                            success: true
+                        });
+                    }
+                }
+            }
+        });
+        app.use(apiRoutes({greenlock}));
+    });
+
+    it('Calls add correctly', async function () {
+        const res = await request(app)
+            .post(getHmac('/api/addDomain'))
+            .send({domain: 'ghost.local'})
+            .set('Accept', 'application/json');
+
+        console.log(res);
+        res.body.success.should.be.true();
+    });
+});
--- test/utils/index.js
+++ test/utils/index.js
@@ -13,6 +13,7 @@ require('./assertions');
 const crypto = require('crypto');
 const fetch = require('node-fetch');
 const querystring = require('querystring');
+const { URLSearchParams } = require('url');
 
 const config = require('../../lib/config');
 const HMAC_KEY = config.get('api:hmac');
@@ -33,6 +34,19 @@ const hmacFetch = (resource, init) => {
     return fetch(url.origin + signedPath, init);
 };
 
+const getHmac = (resource) => {
+    let [path, search] = resource.split('?');
+    if (!search) {
+        search = '';
+    }
+    const params = new URLSearchParams(search);
+    params.set('t', Math.floor(Date.now() / 1000));
+    search = params.toString();
+    const hmac = crypto.createHmac('sha1', HMAC_KEY).update(`${path}?${search}`).digest('base64').replace(/\+/g, '-').replace(/\//g, '_');
+    return `${path}?${search}&hmac=${querystring.escape(hmac)}`;
+};
+
 module.exports = {
-    hmacFetch
+    hmacFetch,
+    getHmac
 };
